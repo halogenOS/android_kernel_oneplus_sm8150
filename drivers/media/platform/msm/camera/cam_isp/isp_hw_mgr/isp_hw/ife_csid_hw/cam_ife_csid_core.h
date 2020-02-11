10d3cccfd07113e300c9563f757e36d096ab9087
Chandan Kumar Jha <cjha@codeaurora.org>
2020-02-11 18:07:49 +0530

    msm: camera: isp: Change state of all CID resource to RESERVE on deinit
    This change is to update state of all acquired CID resources state to
    Reserve on disable CSI2. It will allow, proper release of all CID
    resources even in case multiple CID for single CSID.
    
    Change-Id: I06b7439461388358a83569c1f3e0aa46a083fe6e
    Signed-off-by: Chandan Kumar Jha <cjha@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.h
+++ drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.h
@@ -386,6 +386,7 @@ struct cam_ife_csid_tpg_cfg  {
  * @dt:          Data type
  * @cnt:         Cid resource reference count.
  * @tpg_set:     Tpg used for this cid resource
+ * @init_cnt     cid resource init count
  *
  */
 struct cam_ife_csid_cid_data {
@@ -393,6 +394,7 @@ struct cam_ife_csid_cid_data {
 	uint32_t                     dt;
 	uint32_t                     cnt;
 	uint32_t                     tpg_set;
+	uint32_t                     init_cnt;
 };
 
 
